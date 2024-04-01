6dce11d7446b6c4d72615b2bf35b0f73b4bc0c7b
OKOTB <osamaquotp@gmail.com>
2024-03-31 19:47:59 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+
+#include "monty.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments.
+ * @argv: An array containing the command-line arguments.
+ * Return: Returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	op_file(argv[1]);
+	empty_nodes();
+	return (0);
+}
+
